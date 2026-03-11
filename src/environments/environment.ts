78786bbd06bcc85a4b24e411287fa8af32be3d69
jguerrero74a <jguerrero74@gmail.com>
2026-03-11 00:06:42 -0500

    Ajuste de budgets para estilos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: '',
-  apiKeyGoogle: '',
+  apiUrl: 'https://eccommerce-api-p29d.onrender.com',
+  apiKeyGoogle: 'AIzaSyDdQo939ByN6AhFyABp_-7LWpzqO1o9Fyg',
 };
